c1cd6661e91e04886f01e40474549573d6458fe1
unniisme <122001019@smail.iitpkd.ac.in>
2024-02-07 01:56:58 +0530

    More vercel test

--- Builds/index.py
+++ Builds/index.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test  # type: ignore
-from urllib.parse import urlparse, parse_qs
 from pathlib import Path
 import os
 import sys
 import argparse
 import subprocess
-import ssl
-import json
-
-CERT = "ssl/cert.pem"
-KEY = "ssl/key.pem"
 
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
@@ -20,24 +14,6 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         self.send_header("Access-Control-Allow-Origin", "*")
         super().end_headers()
-
-    def do_POST(self):
-        # Extract the content length
-        content_length = int(self.headers['Content-Length'])
-        # Read the POST data
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        # Parse the POST data
-        post_params = parse_qs(post_data)
-        # Extract the values from the POST parameters
-        text_input1 = post_params.get('textInput1', [''])[0]
-        text_input2 = post_params.get('textInput2', [''])[0]
-        
-        # Set cookies
-        self.send_response(302)
-        self.send_header('Location', '/game.html')
-        self.send_header('Set-Cookie', 'Username={}'.format(text_input1))
-        self.send_header('Set-Cookie', 'IP={}'.format(text_input2))
-        self.end_headers()
 
 
 def shell_open(url):
@@ -58,23 +34,12 @@ def serve(root, port, run_browser):
 
     test(CORSRequestHandler, HTTPServer, port=port)
 
-def serve_secure(root, port, run_browser):
-    os.chdir(root)
-    if run_browser:
-        print("Opening the served URL in the default browser (use `--no-browser` or `-n` to disable this).")
-        shell_open(f"https://127.0.0.1:{port}")
-
-    # Use SSL certificate and key
-    httpd = HTTPServer(('0.0.0.0', port), CORSRequestHandler)
-    httpd.socket = ssl.wrap_socket(httpd.socket, certfile=CERT, keyfile=KEY, server_side=True)
-    httpd.serve_forever()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", help="port to listen on", default=8060, type=int)
+    parser.add_argument("-p", "--port", help="port to listen on", default=5000, type=int)
     parser.add_argument(
-        "-r", "--root", help="path to serve as root (relative to `platform/web/`)", default=".", type=Path
+        "-r", "--root", help="path to serve as root (relative to `platform/web/`)", default="/test", type=Path
     )
     browser_parser = parser.add_mutually_exclusive_group(required=False)
     browser_parser.add_argument(
@@ -87,5 +52,5 @@ if __name__ == "__main__":
     # so that the script can be run from any location.
     os.chdir(Path(__file__).resolve().parent)
 
-    serve_secure(args.root, args.port, args.browser)
+    serve(args.root, args.port, args.browser)
 
