8be7440f96c5f8f1f67641ee4d0a8d9861cb8968
unniisme <122001019@smail.iitpkd.ac.in>
2024-02-09 00:55:10 +0530

    Small fix

--- Builds/index.py
+++ Builds/index.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", help="port to listen on", default=5000, type=int)
     parser.add_argument(
-        "-r", "--root", help="path to serve as root (relative to `platform/web/`)", default="/test", type=Path
+        "-r", "--root", help="path to serve as root (relative to `platform/web/`)", default="./test", type=Path
     )
     browser_parser = parser.add_mutually_exclusive_group(required=False)
     browser_parser.add_argument(
